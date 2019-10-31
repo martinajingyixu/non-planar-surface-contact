277e32b702c8459e8c53db69950ed5d2cc73dc47
Jingyi Xu <jingyi.xu@tum.de>
2019-10-31 15:52:28 +0100

    examples of the pipeline

---
+++ examples.m
@@ -0,0 +1,12 @@
+% Example code to model non-planar surface contacts and its application in
+% grasping. The pipeline includes:
+% 1. Contact profile 
+% 2. Friction computation
+% 3. Fit limit surfaces
+% 4. Multi-contacts and applications in grasping 
+
+%% Contact Profile: surface geometry and pressure distribution
+% 3 types of surface are available: parametric surfaces, meshed surface, 
+% and arbitrary surfaces from FEM simulations
+
+% Example: meshed surface
